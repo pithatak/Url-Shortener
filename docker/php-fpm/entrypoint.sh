d1cf58e5a608af14779526de272ba025750a159f
Ill <poseva41@gmail.com>
2026-01-27 23:34:47 +0100

    Аutomate composer install, migrations. Аdd worker container

---
+++ docker/php-fpm/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -e
+
+echo "Waiting for PostgreSQL..."
+
+until pg_isready -h postgres -p 5432 -U "$POSTGRES_USERNAME"; do
+  sleep 1
+done
+
+echo "PostgreSQL is ready"
+
+echo "🧹 Clearing cache..."
+php bin/console cache:clear || true
+
+# Composer install
+echo "Running composer install..."
+composer install --no-interaction --prefer-dist
+
+echo "Running migrations..."
+php bin/console doctrine:migrations:migrate --no-interaction || true
+
+echo "Starting application..."
+exec "$@"
